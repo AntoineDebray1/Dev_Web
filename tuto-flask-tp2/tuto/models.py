279293b65a425232f207a60424ec16ecf7d77724
Debray Antoine <o22202147@info27-21.iut45.univ-orleans.fr>
2023-10-12 10:24:48 +0200

    Exercice 2 effectué

--- tuto-flask-tp2/tuto/models.py
+++ tuto-flask-tp2/tuto/models.py
@@ -15,5 +15,24 @@ for book in Books:
     i += 1
 
 def get_sample():
-    return Books[0:10]
+    return Book.query.limit(10).all()
+
+from .app import db
+class Author (db.Model ):
+    id = db.Column(db.Integer , primary_key =True)
+    name = db.Column (db.String (100))
+class Book(db.Model ):
+    id = db.Column(db.Integer , primary_key =True)
+    price = db.Column(db.Float)
+    author_id = db.Column(db.Integer , db.ForeignKey ("author.id"))
+    author = db.relationship("Author",backref =db.backref ("books", lazy="dynamic"))
+class url(db.Model ):
+    id = db.Column(db.Integer , primary_key =True)
+    price = db.Column(db.Sting(100))
+class img(db.Model ):
+    id = db.Column(db.Integer , primary_key =True)
+    image = db.Column(db.Sting(100))
+class title(db.Model ):
+    id = db.Column(db.Integer , primary_key =True)
+    price = db.Column(db.Sting(100))
 

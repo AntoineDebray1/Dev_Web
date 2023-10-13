e9ec25ad98d2aaa4b62380337c2c9672f29e79b8
Debray Antoine <o22202147@info04-19.iut45.univ-orleans.fr>
2023-10-13 11:58:10 +0200

    reset de tous les methodes

--- tuto-flask-tp2/tuto/models.py
+++ tuto-flask-tp2/tuto/models.py
@@ -1,38 +1,33 @@
 import yaml, os.path
 
-Books = yaml.safe_load(
-    open(
-        os.path.join(
-            os.path.dirname(__file__),
-            "data.yml"
-            )
-        )
-    )
+# Books = yaml.safe_load(
+#     open(
+#         os.path.join(
+#             os.path.dirname(__file__),
+#             "data.yml"
+#             )
+#         )
+#     )
 
-i = 0
-for book in Books:
-    book['id'] = i 
-    i += 1
+# i = 0
+# for book in Books:
+#     book['id'] = i 
+#     i += 1
+
+
+
+from .app import db
+class Author(db.Model):
+    id = db.Column(db.Integer , primary_key =True)
+    name = db.Column(db.String(100))
+class Book(db.Model):
+    id = db.Column(db.Integer , primary_key =True)
+    price = db.Column(db.Float)
+    title = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    img = db.Column(db.String(100))
+    author_id = db.Column(db.Integer , db.ForeignKey("author.id"))
+    author = db.relationship("Author",backref =db.backref("books", lazy="dynamic"))
 
 def get_sample():
     return Book.query.limit(10).all()
-
-from .app import db
-class Author (db.Model ):
-    id = db.Column(db.Integer , primary_key =True)
-    name = db.Column (db.String (100))
-class Book(db.Model ):
-    id = db.Column(db.Integer , primary_key =True)
-    price = db.Column(db.Float)
-    author_id = db.Column(db.Integer , db.ForeignKey ("author.id"))
-    author = db.relationship("Author",backref =db.backref ("books", lazy="dynamic"))
-class url(db.Model ):
-    id = db.Column(db.Integer , primary_key =True)
-    price = db.Column(db.Sting(100))
-class img(db.Model ):
-    id = db.Column(db.Integer , primary_key =True)
-    image = db.Column(db.Sting(100))
-class title(db.Model ):
-    id = db.Column(db.Integer , primary_key =True)
-    price = db.Column(db.Sting(100))
-
